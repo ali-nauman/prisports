dec952426b822cb49dce2a19d4ec5dcc53cd4677
Ali Nauman Qureshi <alinauman97@outlook.com>
2020-04-29 17:03:28 +0500

    Added player/admin dashboards, a navbar and set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { PlayerDashboardComponent } from './components/player-dashboard/player-dashboard.component';
 
-
-const routes: Routes = [];
+// Add the paths for the components here
+const routes: Routes = [
+  { path: "admin/dashboard", component: AdminDashboardComponent },
+  { path: "player/dashboard", component: PlayerDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
