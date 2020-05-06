89e80dad2183482ef88360c3929428e1180e2512
Ali Nauman Qureshi <alinauman97@outlook.com>
2020-05-06 20:25:53 +0500

    Added mongoose code to index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const express = require('express');
 const http = require('http');
+var mongoose = require('mongoose');
+
+require('./models/user.model');
 
 const accountsRouter = require('./routes/accounts');
 
@@ -13,6 +16,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/accounts', accountsRouter);
+
+var connectionString = 'mongodb://localhost/prisports';
+mongoose.connect(connectionString, { useNewUrlParser: true });
+var dbConnection = mongoose.connection;
+
+db.once('open', () => {
+    console.log(`Connected to ${connectionString}...`);
+});
+dbConnection.on('error', () => {
+    console.error(`Error: Couldn't connect to ${connectionString}`);
+});
 
 const server = http.createServer(app);
 
