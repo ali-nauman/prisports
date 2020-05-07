7d1de354c48408c85c990fb98dd3d585ec6e0a91
alinauman97 <alinauman97@outlook.com>
2020-05-07 16:53:07 +0500

    Completed the authenticate function

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
   baseUrl: string = 'http://localhost:3000';
-  authenticationToken: string;
+  authenticationToken: string = null;
 
   constructor(private http: HttpClient) { }
 
-  authenticate(email: string, password: string) /*: Observable<boolean>*/ {
-    this.http.post(
-      `${this.baseUrl}/accounts/login`,
-      { email: email, password: password },
-      { responseType: 'json', observe: 'body' })
-      .subscribe(response => {
-        console.log(response);
-        // this.authenticationToken = response.success ? response.token : null;
-        // return response.success;
+  authenticate(email: string, password: string) {
+    this.http.post(`${this.baseUrl}/accounts/login`, { email: email, password: password }, { responseType: 'json', observe: 'body' })
+      .subscribe(res => {
+        let response = Object(res);
+        this.authenticationToken = response.success ? response.token : null;
       });
   }
 }
