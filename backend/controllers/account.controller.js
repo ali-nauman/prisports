987a8731a98d8ce8d19e7030f05fe2740959580c
alinauman97 <alinauman97@outlook.com>
2020-05-08 03:09:10 +0500

    Updated the login API to work with the database

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,36 +1,31 @@
 const assert = require('assert');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-// const mongoose = require('mongoose');
+const Users = require('../models/user.model');
 
 const APP_SECRET = "myappsecret";
-const EMAIL = "john.doe@gmail.com"
-let HASHED_PASSWORD;
-
-bcrypt.hash("john", 10, (err, hash) => {
-    HASHED_PASSWORD = hash;
-})
 
 exports.loginUser = (req, res) => {
     console.log(`POST /accounts/login { email: ${req.body.email} }`);
 
-    // To-Do: Search for email in the database
-    if (req.body.email === EMAIL) {
-        bcrypt.compare(req.body.password, HASHED_PASSWORD, (err, same) => {
-            assert.equal(err, null);
+    Users.findOne({ emailAddress: req.body.email }).then((user) => {
+        if (user) {
+            bcrypt.compare(req.body.password, user.password, (err, same) => {
+                assert.equal(err, null);
 
-            if (same) {
-                let token = jwt.sign({ data: EMAIL, expiresIn: '1h' }, APP_SECRET);
-                res.json({ success: true, token: token });
-            }
-            else {
-                res.json({ success: false, message: "Incorrect password" });
-            }
+                if (same) {
+                    let token = jwt.sign({ data: user.email, expiresIn: '1h' }, APP_SECRET);
+                    res.json({ success: true, token: token });
+                }
+                else {
+                    res.json({ success: false, message: "Incorrect password" });
+                }
+                res.end();
+            });
+        }
+        else {
+            res.json({ success: false, message: "User not registered!" });
             res.end();
-        });
-    }
-    else {
-        res.json({ success: false, message: "User not registered!" });
-        res.end();
-    }
+        }
+    });
 };
