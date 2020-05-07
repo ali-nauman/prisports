d495a6e2ea1245b99bf8cc32ffd18301b3f0f2cb
Ali Nauman Qureshi <alinauman97@outlook.com>
2020-05-08 04:45:21 +0500

    Added syntactic sugar

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,31 +1,33 @@
-const assert = require('assert');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
 const Users = require('../models/user.model');
 
 const APP_SECRET = "myappsecret";
 
-exports.loginUser = (req, res) => {
+exports.loginUser = async (req, res) => {
     console.log(`POST /accounts/login { email: ${req.body.email} }`);
 
-    Users.findOne({ emailAddress: req.body.email }).then((user) => {
-        if (user) {
-            bcrypt.compare(req.body.password, user.password, (err, same) => {
-                assert.equal(err, null);
+    try {
+        const user = await Users.findOne({ emailAddress: req.body.email });
 
-                if (same) {
-                    let token = jwt.sign({ data: user.email, expiresIn: '1h' }, APP_SECRET);
-                    res.json({ success: true, token: token });
-                }
-                else {
-                    res.json({ success: false, message: "Incorrect password" });
-                }
-                res.end();
-            });
+        if (user) {
+            const isPasswordSame = await bcrypt.compare(req.body.password, user.password);
+
+            if (isPasswordSame) {
+                let token = jwt.sign({ data: user.email, expiresIn: '1h' }, APP_SECRET);
+                res.json({ success: true, token: token });
+            }
+            else {
+                res.json({ success: false, message: "Incorrect password" });
+            }
+            res.end();
         }
         else {
             res.json({ success: false, message: "User not registered!" });
             res.end();
         }
-    });
-};
+    } catch (err) {
+        console.error(err);
+    }
+}
