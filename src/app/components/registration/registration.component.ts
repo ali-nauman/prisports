19646fe19627237f9dac3df7561884a221beb0e9
alinauman97 <alinauman97@outlook.com>
2020-05-07 13:11:28 +0500

    Added FormGroup to Registration component

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private restService: RestService) {
+    this.registrationForm = this.formBuilder.group(
+      {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      });
+   }
 
   ngOnInit(): void {
+  }
+
+  register() {
+    
   }
 
 }
