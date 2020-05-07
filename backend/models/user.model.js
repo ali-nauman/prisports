e2057a36ca309343dbb0d3c8b5992b1a7563f167
kulsoomMujtaba <kulsoom.mujtaba@gmail.com>
2020-05-08 01:30:18 +0500

    User Schema updated

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 var userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    emailAddress: String,
-    hashedPassword: String,
-    phoneNumber: String,
+    firstName: {type: String, required: [true,"This field cannot be empty"]},
+    lastName: {type: String, required: [true,"This field cannot be empty"]},
+    emailAddress: {type: String, lowercase: true, unique: true, required: [true, "This field cannot be empty"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+    hashedPassword: {type: String, required: [true,"This field cannot be empty"]},
+    phoneNumber: {type: String, required: [true,"This field cannot be empty"]},
     role: String
 });
 
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 mongoose.model('User', userSchema);
+UserSchema.plugin(AutoIncrement, {inc_field: 'id'});
