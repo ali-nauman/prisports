882309fc99c1d9e6ccc00b808a59300b87f52e71
alinauman97 <alinauman97@outlook.com>
2020-05-08 03:09:38 +0500

    Added a database seeder module

---
+++ backend/database/databaseSeeder.js
@@ -0,0 +1,21 @@
+const Users = require('../models/user.model');
+
+function seedDatabase() {
+    Users.find({}).then((users) => {
+        if (users.length == 0) {
+            console.log("Users collection empty, adding users...")
+            Users.create({
+                firstName: "John",
+                lastName: "Doe",
+                emailAddress: "john.doe@gmail.com",
+                password: "$2b$10$qiDK.uihjQmt7jhJa2wG9eK34zfFGCF6i9x9appiVp8LuXCEPdkFK",
+                phoneNumber: "123456789",
+                role: "Player"
+            }).then((user) => {
+                console.log("Created user: ", user);
+            })
+        }
+    })
+}
+
+module.exports = seedDatabase;
