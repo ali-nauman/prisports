c9fe9e08e4032e958e0aff3a7360a9d2248054b5
alinauman97 <alinauman97@outlook.com>
2020-05-08 01:44:19 +0500

    Fixed the User model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,16 +3,17 @@ var uniqueValidator = require('mongoose-unique-validator');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 var userSchema = new mongoose.Schema({
-    firstName: {type: String, required: [true,"This field cannot be empty"]},
-    lastName: {type: String, required: [true,"This field cannot be empty"]},
-    emailAddress: {type: String, lowercase: true, unique: true, required: [true, "This field cannot be empty"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-    hashedPassword: {type: String, required: [true,"This field cannot be empty"]},
-    phoneNumber: {type: String, required: [true,"This field cannot be empty"]},
-    role: {type: String, required: [true,"This field cannot be empty"]}
+    firstName: { type: String, required: [true, "This field cannot be empty"] },
+    lastName: { type: String, required: [true, "This field cannot be empty"] },
+    emailAddress: { type: String, lowercase: true, unique: true, required: [true, "This field cannot be empty"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
+    password: { type: String, required: [true, "This field cannot be empty"] },
+    phoneNumber: { type: String, required: [true, "This field cannot be empty"] },
+    role: { type: String, required: [true, "This field cannot be empty"] }
 });
 
-UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
-mongoose.model('User', userSchema);
-UserSchema.plugin(AutoIncrement, {inc_field: 'id'});
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+UserSchema.plugin(AutoIncrement, { inc_field: 'id' });
 
-module.exports = userSchema;
+let Users = mongoose.model('User', userSchema);
+
+module.exports = Users;
