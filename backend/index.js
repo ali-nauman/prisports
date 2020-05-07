e2d074b14ab6d5cccb24099018acf1fb6fba06f0
alinauman97 <alinauman97@outlook.com>
2020-05-07 16:52:20 +0500

    Removed body-parser dependency (not needed)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const express = require('express');
 const http = require('http');
 var mongoose = require('mongoose');
@@ -12,7 +11,7 @@ const hostname = 'localhost';
 const port = 3000;
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.use('/accounts', accountsRouter);
@@ -25,11 +24,11 @@ dbConnection.once('open', () => {
     console.log(`Connected to ${connectionString}...`);
 });
 dbConnection.on('error', () => {
-    console.error(`Error: Couldn't connect to ${connectionString}`);
+    console.error(`Error: Couldn't connect to ${connectionString}!`);
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/...`);
 });
