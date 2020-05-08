4c9a0e3c9a404ff4079c3952cfde6dc2e11c30ba
alinauman97 <alinauman97@outlook.com>
2020-05-08 22:09:21 +0500

    Added input for phone number, and some other tweaks

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -11,29 +11,34 @@ export class RegistrationComponent implements OnInit {
   sports = ['Tennis', 'Badmintion', 'Table-tennis', 'Squash'];
   registrationForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private restService: RestService) {   }
+  constructor(private formBuilder: FormBuilder, private restService: RestService) { }
 
   ngOnInit(): void {
     this.registrationForm = this.formBuilder.group(
       {
         firstName: '',
         lastName: '',
-        email: '',
+        emailAddress: '',
+        phoneNumber: '',
         password: '',
-        confirmPassword: '',
-        sport1: '0',
-        rank1: '0'
+        confirmedPassword: '',
+        sport1: '',
+        rank1: ''
       });
   }
 
   register() {
     let firstname = this.registrationForm.get('firstName').value;
     let lastname = this.registrationForm.get('lastName').value;
-    let email = this.registrationForm.get('email').value;
+    let emailAddress = this.registrationForm.get('emailAddress').value;
+    let phoneNumber = this.registrationForm.get('phoneNumber').value;
     let password = this.registrationForm.get('password').value;
+    let confirmedPassword = this.registrationForm.get('confirmedPassword').value;
     let sport1 = this.registrationForm.get('sport1').value;
     let rank1 = this.registrationForm.get('rank1').value;
-    this.restService.registerUser(firstname, lastname, email, password, sport1, rank1);
-    // console.log(firstname, lastname, mail, pswrd, sport1,rank1);
+
+    if (password == confirmedPassword) {
+      this.restService.registerUser(firstname, lastname, emailAddress, phoneNumber, password, sport1, rank1);
+    }
   }
 }
