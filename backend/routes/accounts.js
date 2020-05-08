58000e3a4bbfd0b13cd3ef9d2d5e4260833d93ae
alinauman97 <alinauman97@outlook.com>
2020-05-08 22:11:25 +0500

    Added routing for /register endpoint

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -5,5 +5,6 @@ const accountController = require('../controllers/account.controller');
 const accountRouter = express.Router();
 
 accountRouter.post('/login', accountController.loginUser);
+accountRouter.post('/register', accountController.registerUser);
 
 module.exports = accountRouter;
