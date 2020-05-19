50e574dfc0170cd562034b2b87f232e1b3cd8459
alinauman97 <alinauman97@outlook.com>
2020-05-19 23:07:01 +0500

    Created PracticeSession model

---
+++ backend/models/practiceSession.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+// To-Do: Create court model, import here and uncomment the reference
+const practiceSessionSchema = new mongoose.Schema({
+    courtId: { type: mongoose.Types.ObjectId /* , ref: Court */ },
+    coachId: { type: mongoose.Types.ObjectId, ref: User },
+    startTime: { type: Date },
+    endTime: { type: Date },
+    opponentId: { type: mongoose.Types.ObjectId, ref: User }
+});
+
+practiceSessionSchema.plugin(AutoIncrement, { inc_field: 'id' });
+
+module.exports = mongoose.model('PracticeSession', practiceSessionSchema);
