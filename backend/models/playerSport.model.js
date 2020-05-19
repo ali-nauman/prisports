4c2d0c2316451a38fb1cb22c617304db6850a64c
alinauman97 <alinauman97@outlook.com>
2020-05-19 23:07:29 +0500

    Created PlayerSport model

---
+++ backend/models/playerSport.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const User = require('./user.model');
+const Sport = require('./sport.model');
+
+const playerSportSchema = new mongoose.Schema({
+    playerId: { type: mongoose.Types.ObjectId, ref: User, required: true },
+    sportId: { type: mongoose.Types.ObjectId, ref: Sport, required: true }
+});
+
+playerSportSchema.plugin(AutoIncrement, { inc_field: 'id' });
+
+module.exports = mongoose.model('PlayerSport', playerSportSchema);
