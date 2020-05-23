e3f13320ec9ad5b09d1b4d6df55ece531b79cf27
alinauman97 <alinauman97@outlook.com>
2020-05-23 20:02:49 +0500

    Added morgan and passport modules

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const cors = require('cors');
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+const passport = require('passport');
 
 const database = require('./database/database');
 const accountsRouter = require('./routes/accounts');
@@ -10,8 +12,10 @@ const hostname = 'localhost';
 const port = 3000;
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+app.use(passport.initialize());
 
 app.use('/accounts', accountsRouter);
 app.use('/coaches', coachesRouter);
