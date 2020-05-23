a4efadac2cb77e811bf2815202a590b71ff34314
alinauman97 <alinauman97@outlook.com>
2020-05-23 20:10:27 +0500

    Added middleware to verify that the request comes from authenticated users only

--- backend/routes/coaches.js
+++ backend/routes/coaches.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
+const authenticate = require('../authenticate');
 const coachController = require('../controllers/coach.controller');
 
 const coachRouter = express.Router();
 
-coachRouter.get('/:coachId/practiceSessions', coachController.getPracticeSessions);
-coachRouter.get('/:coachId/matches', coachController.getMatches);
+coachRouter.get('/:coachId/matches', authenticate.verifyUser, authenticate.verifyCoach, coachController.getMatches);
+coachRouter.get('/:coachId/practiceSessions', authenticate.verifyUser, authenticate.verifyCoach, coachController.getPracticeSessions);
 
 module.exports = coachRouter;
