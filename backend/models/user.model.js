ba81d077f3bb5ec49b15856555f7692ab2ca348e
alinauman97 <alinauman97@outlook.com>
2020-05-23 20:08:44 +0500

    Updated to work with passport (removed password)

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: [true, "This field cannot be empty"] },
     lastName: { type: String, required: [true, "This field cannot be empty"] },
     emailAddress: { type: String, lowercase: true, unique: true, required: [true, "This field cannot be empty"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
     phoneNumber: { type: String, required: [true, "This field cannot be empty"] },
-    password: { type: String, required: [true, "This field cannot be empty"] },
     role: { type: String, required: [true, "This field cannot be empty"] }
 });
 
+userSchema.plugin(passportLocalMongoose, { usernameField: 'emailAddress' });
 userSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 userSchema.plugin(AutoIncrement, { inc_field: 'id' });
 
