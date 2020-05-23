0c2ae17aed0605b8ce7348696c2d7059b59c9290
alinauman97 <alinauman97@outlook.com>
2020-05-23 20:05:08 +0500

    Updated the functions to work with passport

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,62 +1,42 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
-const Users = require('../models/user.model');
+const authenticate = require('../authenticate');
+const User = require('../models/user.model');
 
-const APP_SECRET = "myappsecret";
+exports.loginUser = (req, res) => {
+    var token = authenticate.getToken({ _id: req.user._id });
 
-exports.loginUser = async (req, res) => {
-    console.log(`POST /accounts/login { email: ${req.body.emailAddress} }`);
-
-    try {
-        const user = await Users.findOne({ emailAddress: req.body.emailAddress });
-
-        if (user) {
-            const isPasswordSame = await bcrypt.compare(req.body.password, user.password);
-
-            if (isPasswordSame) {
-                let token = jwt.sign({ data: user.email, expiresIn: '1h' }, APP_SECRET);
-                res.json({ success: true, token: token });
-            }
-            else {
-                res.json({ success: false, message: "Incorrect password" });
-            }
-            res.end();
-        }
-        else {
-            res.json({ success: false, message: "User not registered!" });
-            res.end();
-        }
-    } catch (err) {
-        console.error(err);
-    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        success: true,
+        status: 'You are successfully logged in!',
+        token: token
+    });
 }
 
 exports.registerUser = async (req, res) => {
-    console.log(`POST /accounts/register { email: ${req.body.emailAddress} }`);
-
     try {
-        const user = await Users.findOne({ emailAddress: req.body.emailAddress });
-        if (user) {
-            res.json({ success: false, message: "This email already exists. Please use another email ID" });
-            res.end();
-        }
-        else {
-            let hashedPassword = await bcrypt.hash(req.body.password, 10);
-            let user = await Users.create({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                emailAddress: req.body.emailAddress,
-                phoneNumber: req.body.phoneNumber,
-                password: hashedPassword,
-                role: "Player"
-            });
+        await User.register(new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            emailAddress: req.body.emailAddress,
+            phoneNumber: req.body.phoneNumber,
+            role: "Player"
+        }), req.body.password);
 
-            console.log("Created user: ", user);
-            res.json(user);
-            res.end();
-        }
-    } catch (err) {
-        console.error(err);
+        passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+                success: true,
+                status: 'Registration Successful!'
+            });
+        });
+    } catch (error) {
+        console.error(error);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: error });
     }
 }
