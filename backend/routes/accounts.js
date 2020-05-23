59acab0089f97e335bbfde58a3ee722566969d44
alinauman97 <alinauman97@outlook.com>
2020-05-23 20:09:25 +0500

    Added passport.authenticate() middleware in the login request handling

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 
 const accountController = require('../controllers/account.controller');
 
 const accountRouter = express.Router();
 
-accountRouter.post('/login', accountController.loginUser);
+accountRouter.post('/login', passport.authenticate('local'), accountController.loginUser);
 accountRouter.post('/register', accountController.registerUser);
 
 module.exports = accountRouter;
