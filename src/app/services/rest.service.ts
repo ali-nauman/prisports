0ef7222f2136748a599dc2a49a2acaad9bfe4312
Ali Nauman Qureshi <alinauman97@outlook.com>
2020-05-25 17:30:01 +0500

    Added getMatches() and getPracticeSessions()

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,28 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
-  baseUrl: string = 'http://localhost:3000';
-  authenticationToken: string = null;
+  private baseUrl: string = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 
-  authenticate(emailAddress: string, password: string) {
-    this.http.post(`${this.baseUrl}/accounts/login`, { emailAddress: emailAddress, password: password }, { responseType: 'json', observe: 'body' })
-      .subscribe(res => {
-        let response = Object(res);
-        this.authenticationToken = response.success ? response.token : null;
-      });
+  authenticate(emailAddress: string, password: string): any {
+    return this.http.post(`${this.baseUrl}/accounts/login`, { emailAddress: emailAddress, password: password }, { responseType: 'json', observe: 'body' })
   }
 
-  registerUser(firstName: string, lastName: string, emailAddress: string, phoneNumber: string, password: string, sport1: string, rank1: string ){
-    this.http.post<any>(`${this.baseUrl}/accounts/register`, {
+  registerUser(firstName: string, lastName: string, emailAddress: string, phoneNumber: string, password: string, sport1: string, rank1: string): any {
+    return this.http.post<any>(`${this.baseUrl}/accounts/register`, {
       firstName: firstName, lastName: lastName, emailAddress: emailAddress, phoneNumber: phoneNumber, password: password,
-      sport1: sport1, rank1: rank1 }, { responseType: 'json', observe: 'body' })
-      .subscribe(res => {});
+      sport1: sport1, rank1: rank1
+    }, { responseType: 'json', observe: 'body' });
+  }
+
+  getMatches() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `bearer ${localStorage.getItem('token')}`
+      })
+    };
+    return this.http.get<any>(`${this.baseUrl}/coaches/matches`, httpOptions);
+  }
+
+  getPracticeSessions() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `bearer ${localStorage.getItem('token')}`
+      })
+    };
+    return this.http.get<any>(`${this.baseUrl}/coaches/practiceSessions`, httpOptions);
   }
 }
