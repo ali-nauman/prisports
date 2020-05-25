afe25007937f6a01fbddc021e80885937e1138f9
Ali Nauman Qureshi <alinauman97@outlook.com>
2020-05-25 16:49:35 +0500

    Added player ranks

--- backend/models/practice.session.model.js
+++ backend/models/practice.session.model.js
@@ -5,7 +5,9 @@ const practiceSessionSchema = new mongoose.Schema({
     courtId: { type: mongoose.Types.ObjectId, ref: 'Court', required: true },
     coachId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
     playerAId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    playerARank: { type: String, required: true },
     playerBId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+    playerBRank: { type: String, required: true },
     startTime: { type: Date, required: true },
     endTime: { type: Date, required: true }
 });
