b9817eaf84b94731cc8c24e7c13d306c2950ba55
Ali Nauman Qureshi <alinauman97@outlook.com>
2020-05-25 17:31:13 +0500

    Saved token to localStorage, navigated to different pages

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { RestService } from 'src/app/services/rest.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,22 +9,31 @@ import { RestService } from 'src/app/services/rest.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm : FormGroup;
+  loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private restService: RestService) {
+  constructor(private formBuilder: FormBuilder, private restService: RestService, private router: Router) {
     this.loginForm = this.formBuilder.group(
       {
         email: '',
         password: ''
       });
-   }
+  }
 
   login() {
     let email = this.loginForm.get('email').value;
     let password = this.loginForm.get('password').value;
 
-    this.restService.authenticate(email, password);
-   }
+    this.restService.authenticate(email, password).subscribe(
+      res => {
+        if (res.success) {
+          localStorage.setItem('token', res.token);
+          if (res.role == "Admin") { this.router.navigate(['admin/dashboard']); }
+          else if (res.role == "Coach") { this.router.navigate(['coach/dashboard']); }
+          else { this.router.navigate(['player/dashboard']); }
+        }
+      },
+      err => console.error(err));
+  }
 
   ngOnInit(): void {
   }
