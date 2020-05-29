7ac74b74fe66228cf2b1ba71f2883a688f73f466
Hanzla Haroon <hanzla.haroon@outlook.com>
2020-05-29 15:15:00 +0500

    Updated getMatches and getPracticeSession in PlayerController

--- backend/controllers/player.controller.js
+++ backend/controllers/player.controller.js
@@ -1,36 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// const User = mongoose.model('User');
-const Match = mongoose.model('Match');
-const PracticeSession = mongoose.model('PracticeSession');
-
+const Match = mongoose.model("Match");
+const PracticeSession = mongoose.model("PracticeSession");
+// playerAId: req.playerAId
 exports.getMatches = async (req, res, next) => {
-    try {
-        const matches = await Match.find({ playerId: req.user._id })
-            .populate('courtId', { name: 1, _id: 0 })
-            .populate('playerAId', { firstName: 1, lastName: 1, _id: 0 })
-            .populate('playerBId', { firstName: 1, lastName: 1, _id: 0 })
-            .populate('coachId', { firstName: 1, lastName: 1, _id: 0 });
-            // .populate('startTime');
-
-        res.statusCode = 200;
-        res.json(matches);
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    const matches = await Match.find({
+      $or: [{ playerAId: req.user._id }, { playerBId: req.user._id }],
+    })
+      .populate("courtId", { name: 1, _id: 0 })
+      .populate("playerAId", { firstName: 1, lastName: 1, _id: 0 })
+      .populate("playerBId", { firstName: 1, lastName: 1, _id: 0 })
+      .populate("coachId", { firstName: 1, lastName: 1, _id: 0 });
+ 
+    res.statusCode = 200;
+    res.json(matches);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 exports.getPracticeSessions = async (req, res, next) => {
-    try {
-        const practiceSessions = await PracticeSession.find({ playerId: req.user._id })
-            .populate('courtId', { name: 1, _id: 0 })
-            .populate('playerAId', { firstName: 1, lastName: 1, _id: 0 })
-            .populate('playerBId', { firstName: 1, lastName: 1, _id: 0 })
-            .populate('coachId', { firstName: 1, lastName: 1, _id: 0 });
+  try {
+    const practiceSessions = await PracticeSession.find({
+        $or: [{ playerAId: req.user._id }, { playerBId: req.user._id }],
+      })
+      .populate("courtId", { name: 1, _id: 0 })
+      .populate("playerAId", { firstName: 1, lastName: 1, _id: 0 })
+      .populate("playerBId", { firstName: 1, lastName: 1, _id: 0 })
+      .populate("coachId", { firstName: 1, lastName: 1, _id: 0 });
 
-        res.statusCode = 200;
-        res.json(practiceSessions);
-    } catch (err) {
-        console.error(err);
-    }
-}
+    res.statusCode = 200;
+    res.json(practiceSessions);
+  } catch (err) {
+    console.error(err);
+  }
+};
