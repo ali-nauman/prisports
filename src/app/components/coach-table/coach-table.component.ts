5a1e2ad40762770f3b39ab3eb8aa93d832966a1b
alinauman97 <alinauman97@outlook.com>
2020-06-01 16:37:47 +0500

    Assigned a handler to the Assign Ranks button, added code to update the table on refresh

--- src/app/components/coach-table/coach-table.component.ts
+++ src/app/components/coach-table/coach-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CoachAssignRanksModalComponent } from '../coach-assign-ranks-modal/coach-assign-ranks-modal.component';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-coach-table',
@@ -7,10 +9,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./coach-table.component.css']
 })
 export class CoachTableComponent implements OnInit {
+  @Input() displaying: any;
   @Input() rows: any[];
 
-  constructor(private router: Router) { }
+  constructor(
+    private modalService: NgbModal,
+    private restService: RestService) { }
 
   ngOnInit(): void {
+    if (this.displaying == "matches") {
+      this.restService.getMatches().subscribe(res => this.rows = res);
+    } else {
+      this.restService.getPracticeSessions().subscribe(res => this.rows = res);
+    }
+  }
+
+  assignRanks(row) {
+    const ref = this.modalService.open(CoachAssignRanksModalComponent);
+    ref.componentInstance.row = row;
+    ref.componentInstance.displaying = this.displaying;
+
+    ref.result.then(yes => {
+      console.log("Ok clicked!")
+    }, cancel => {
+      console.log("Cancel clicked!");
+    });
   }
 }
