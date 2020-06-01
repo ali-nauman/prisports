37fa772433310457cbdbb8c7a5e27a1688a60b02
alinauman97 <alinauman97@outlook.com>
2020-06-01 10:18:02 +0500

    Added logic to iterate over the sports in the req body and add them to the database

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -3,6 +3,10 @@ const passport = require('passport');
 
 const authenticate = require('../authenticate');
 const User = mongoose.model('User');
+const Rank = mongoose.model('Rank');
+// const PlayerSport = mongoose.model('PlayerSport');
+const PlayerSport = require('../models/player.sport.model');
+const Sport = mongoose.model('Sport');
 
 exports.logIn = (req, res) => {
     var token = authenticate.getToken({ _id: req.user._id });
@@ -28,13 +32,24 @@ exports.logOut = (req, res) => {
 
 exports.register = async (req, res) => {
     try {
-        await User.register(new User({
+        let player = await User.register(new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             emailAddress: req.body.emailAddress,
             phoneNumber: req.body.phoneNumber,
             role: "Player"
         }), req.body.password);
+
+        req.body.sports.forEach(async (sport) => {
+            try {
+                let sportId = (await Sport.findOne({ name: sport.name }))._id;
+                let rankId = (await Rank.findOne({ name: sport.rank }))._id;
+
+                await PlayerSport.create({ playerId: player._id, sportId: sportId, rankId: rankId });
+            } catch (error) {
+                console.error(error);
+            }
+        });
 
         passport.authenticate('local')(req, res, () => {
             res.statusCode = 200;
