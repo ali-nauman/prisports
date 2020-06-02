c2dd7011a93b940a8668978ff46991410f932936
Hanzla Haroon <hanzla.haroon@outlook.com>
2020-06-02 19:57:46 +0500

    updated admin-user-table component

--- src/app/components/admin-user-table/admin-user-table.component.ts
+++ src/app/components/admin-user-table/admin-user-table.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-admin-user-table',
   templateUrl: './admin-user-table.component.html',
-  styleUrls: ['./admin-user-table.component.css']
+  styleUrls: ['./admin-user-table.component.css'],
 })
 export class AdminUserTableComponent implements OnInit {
+  @Input() displaying: any;
+  @Input() rows: any[];
 
-  constructor() { }
+  constructor(
+    private restService: RestService
+  ) {}
 
   ngOnInit(): void {
+    this.populateTable();
   }
 
+  populateTable(): void {
+    if (this.displaying == "players") {
+      this.restService.getPlayers().subscribe(res => this.rows = res);
+    }
+    else {
+      this.restService.getCoaches().subscribe(res => this.rows = res);
+    }
+  }
+
+  editPlayer(): void{}
+
+  deletePlayer(): void{}
+
+  createPlayer(): void{}
 }
