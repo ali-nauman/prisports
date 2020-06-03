199f4452cb1c2d627dba1619e414090814029d3b
Hanzla Haroon <hanzla.haroon@outlook.com>
2020-06-03 20:19:21 +0500

    added endpoints for updating and deleting user

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
+// const PlayerSport = mongoose.model('PlayerSport');
 
 exports.getPlayers = async (req, res, next) => {
     try {
@@ -9,6 +10,49 @@ exports.getPlayers = async (req, res, next) => {
         res.json(players);
     } catch (err) {
         console.error(err);
+    }
+}
+
+exports.getPlayer = async (req, res, next) => {
+    try {
+        const player = await User.findOne({ _id: req.params.userId });
+        res.statusCode = 200;
+        res.json(player);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+exports.updatePlayer = async (req, res, next) => {
+    console.log("exports.updatePlayer -> req", req)
+    try {
+        const player = await User.findOneAndUpdate({
+            _id: req.params.playerId
+        }, {
+            $set: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                emailAddress: req.body.emailAddress,
+                phoneNumber: req.body.phoneNo,
+                role: req.body.role
+            }
+        });
+        res.statusCode = 200;
+        res.json(player);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+exports.deletePlayer = async (req, res, next) => {
+    console.log("exports.deletePlayer -> req.params.playerId", req.params.playerId)
+    try {
+        const deleteStatus = await User.findOneAndRemove({_id: req.params.playerId});
+        res.statusCode = 200;
+        res.json(deleteStatus);
+    } catch (err) {
+        console.log(err);
+        return next(err);
     }
 }
 
--- backend/routers/admin.router.js
+++ backend/routers/admin.router.js
@@ -6,6 +6,10 @@ const adminController = require('../controllers/admin.controller');
 const adminRouter =  express.Router();
 
 adminRouter.get('/players', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getPlayers);
+adminRouter.get('/player/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getPlayer);
+adminRouter.put('/updatePlayer/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.updatePlayer);
+adminRouter.delete('/deletePlayer/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.deletePlayer);
+
 adminRouter.get('/coaches', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getCoaches);
 
 module.exports = adminRouter;
