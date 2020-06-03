a57faaa82bb58a32d9178eead4c1f489824397d7
Hanzla Haroon <hanzla.haroon@outlook.com>
2020-06-03 20:31:01 +0500

    added implementation in admin-update-user

--- src/app/components/admin-update-user/admin-update-user.component.ts
+++ src/app/components/admin-update-user/admin-update-user.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-admin-update-user',
   templateUrl: './admin-update-user.component.html',
-  styleUrls: ['./admin-update-user.component.css']
+  styleUrls: ['./admin-update-user.component.css'],
 })
 export class AdminUpdateUserComponent implements OnInit {
+  updateUserForm: FormGroup;
+  row: any;
+  displaying: string;
+  // roles = ['player', 'coach', 'admin'];
 
-  constructor() { }
+  constructor(
+    public modal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private restService: RestService
+  ) {}
 
   ngOnInit(): void {
+    this.updateUserForm = this.formBuilder.group({
+      firstName: [this.row.firstName],
+      lastName: [this.row.lastName],
+      email: [this.row.emailAddress],
+      phone: [this.row.phoneNumber],
+      role: [this.row.role],
+      // password: [this.row.password],
+    });
   }
 
+  updatePlayer(): void {
+    this.restService.setPlayerData(this.row._id, this.updateUserForm.value.firstName, this.updateUserForm.value.lastName, 
+      this.updateUserForm.value.email, this.updateUserForm.value.phone, this.updateUserForm.value.role).subscribe(this.modal.close);
+  }
 }
