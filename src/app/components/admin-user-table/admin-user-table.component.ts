f8363e76c2af6d832089f280f487e77487328a96
alinauman97 <alinauman97@outlook.com>
2020-06-05 03:15:52 +0500

    Reused to display coaches

--- src/app/components/admin-user-table/admin-user-table.component.ts
+++ src/app/components/admin-user-table/admin-user-table.component.ts
@@ -4,20 +4,19 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AdminUpdateUserComponent } from '../admin-update-user/admin-update-user.component';
 import { AdminCreateUserComponent } from "../admin-create-user/admin-create-user.component";
 
-
 @Component({
   selector: 'app-admin-user-table',
   templateUrl: './admin-user-table.component.html',
   styleUrls: ['./admin-user-table.component.css'],
 })
 export class AdminUserTableComponent implements OnInit {
-  @Input() displaying: any;
-  @Input() rows: any[];
+  @Input() displaying: string;
+  @Input() users: any[];
 
   constructor(
     private restService: RestService,
     private modalService: NgbModal
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.populateTable();
@@ -25,31 +24,37 @@ export class AdminUserTableComponent implements OnInit {
 
   populateTable(): void {
     if (this.displaying == "players") {
-      this.restService.getPlayers().subscribe(res => this.rows = res);
+      this.restService.getPlayers().subscribe(res => this.users = res);
     }
     else {
-      this.restService.getCoaches().subscribe(res => this.rows = res);
+      this.restService.getCoaches().subscribe(res => this.users = res);
     }
   }
 
-  editPlayer(row: any): void{
+  editUser(user: any): void {
     const ref = this.modalService.open(AdminUpdateUserComponent);
-    ref.componentInstance.row = row;
-    ref.componentInstance.displaying = this.displaying;
+    ref.componentInstance.user = user;
+    ref.componentInstance.adding = this.displaying;
 
     ref.result.then(onFulFilled => {
       this.populateTable();
     });
   }
 
-  deletePlayer(row: any): void{
-   this.restService.deletePlayer(row._id).subscribe( res => this.rows.splice(row._id,1));
-   this.populateTable();
+  deleteUser(user: any): void {
+    if (this.displaying == "players") {
+      this.restService.deletePlayer(user._id).subscribe(res => this.users.splice(user._id, 1));
+    }
+    else {
+      this.restService.deleteCoach(user._id).subscribe(res => this.users.splice(user._id, 1));
+    }
+
+    this.populateTable();
   }
 
-  createPlayer(): void{
+  createUser(): void {
     const ref = this.modalService.open(AdminCreateUserComponent);
-    ref.componentInstance.displaying = this.displaying;
+    ref.componentInstance.adding = this.displaying;
 
     ref.result.then(onFulFilled => {
       this.populateTable();
