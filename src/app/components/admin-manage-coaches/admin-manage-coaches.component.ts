69c7ba169d7edd2a101324586036dc829ad8c9ab
alinauman97 <alinauman97@outlook.com>
2020-06-05 03:13:14 +0500

    Implemented this component

--- src/app/components/admin-manage-coaches/admin-manage-coaches.component.ts
+++ src/app/components/admin-manage-coaches/admin-manage-coaches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-admin-manage-coaches',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-manage-coaches.component.css']
 })
 export class AdminManageCoachesComponent implements OnInit {
+  coaches: any;
 
-  constructor() { }
+  constructor(private restService: RestService) { }
 
   ngOnInit(): void {
+    this.restService.getCoaches().subscribe(
+      res => this.coaches = res,
+      err => console.error(err));
   }
-
 }
