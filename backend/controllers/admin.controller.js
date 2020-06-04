03d9d2c7b876b53707c3d1c8bcd20c24da5f220b
alinauman97 <alinauman97@outlook.com>
2020-06-05 03:09:47 +0500

    Added functions to support PUT, POST and DELETE requests at /admins/coaches[/:coachId]

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -1,7 +1,66 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
-// const PlayerSport = mongoose.model('PlayerSport');
+
+exports.addCoach = async (req, res, next) => {
+    try {
+        const coach = await User.register({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            emailAddress: req.body.emailAddress,
+            phoneNumber: req.body.phoneNumber,
+            role: "Coach"
+        }, req.body.password)
+        res.statusCode = 200;
+        res.json(coach);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+}
+
+exports.deleteCoach = async (req, res, next) => {
+    try {
+        const deleteStatus = await User.findOneAndRemove({ _id: req.params.coachId });
+        res.statusCode = 200;
+        res.json(deleteStatus);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+}
+
+exports.deletePlayer = async (req, res, next) => {
+    try {
+        const deleteStatus = await User.findOneAndRemove({ _id: req.params.playerId });
+        res.statusCode = 200;
+        res.json(deleteStatus);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+}
+
+exports.getCoaches = async (req, res, next) => {
+    try {
+        const coaches = await User.find({ role: 'Coach' });
+        res.statusCode = 200;
+        res.json(coaches);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+}
+
+// exports.getPlayer = async (req, res, next) => {
+//     try {
+//         const player = await User.findOne({ _id: req.params.userId });
+//         res.statusCode = 200;
+//         res.json(player);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
 
 exports.getPlayers = async (req, res, next) => {
     try {
@@ -10,21 +69,31 @@ exports.getPlayers = async (req, res, next) => {
         res.json(players);
     } catch (err) {
         console.error(err);
+        next(err);
     }
 }
 
-exports.getPlayer = async (req, res, next) => {
+exports.updateCoach = async (req, res, next) => {
     try {
-        const player = await User.findOne({ _id: req.params.userId });
+        const coach = await User.findOneAndUpdate({
+            _id: req.params.coachId
+        }, {
+            $set: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                emailAddress: req.body.emailAddress,
+                phoneNumber: req.body.phoneNumber
+            }
+        });
         res.statusCode = 200;
-        res.json(player);
+        res.json(coach);
     } catch (err) {
-        console.error(err);
+        console.log(err);
+        next(err);
     }
 }
 
 exports.updatePlayer = async (req, res, next) => {
-    console.log("exports.updatePlayer -> req", req)
     try {
         const player = await User.findOneAndUpdate({
             _id: req.params.playerId
@@ -33,35 +102,13 @@ exports.updatePlayer = async (req, res, next) => {
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
                 emailAddress: req.body.emailAddress,
-                phoneNumber: req.body.phoneNo,
-                role: req.body.role
+                phoneNumber: req.body.phoneNumber
             }
         });
         res.statusCode = 200;
         res.json(player);
     } catch (err) {
         console.log(err);
-    }
-}
-
-exports.deletePlayer = async (req, res, next) => {
-    console.log("exports.deletePlayer -> req.params.playerId", req.params.playerId)
-    try {
-        const deleteStatus = await User.findOneAndRemove({_id: req.params.playerId});
-        res.statusCode = 200;
-        res.json(deleteStatus);
-    } catch (err) {
-        console.log(err);
-        return next(err);
-    }
-}
-
-exports.getCoaches = async (req, res, next) => {
-    try {
-        const coach = await User.find({ role: 'Coach' });
-        res.statusCode = 200;
-        res.json(coach);
-    } catch (err) {
-        console.error(err);
+        next(err);
     }
 }
--- backend/routers/admin.router.js
+++ backend/routers/admin.router.js
@@ -3,13 +3,16 @@ const express = require('express');
 const authenticate = require('../authenticate');
 const adminController = require('../controllers/admin.controller');
 
-const adminRouter =  express.Router();
-
-adminRouter.get('/players', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getPlayers);
-adminRouter.get('/player/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getPlayer);
-adminRouter.put('/updatePlayer/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.updatePlayer);
-adminRouter.delete('/deletePlayer/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.deletePlayer);
+const adminRouter = express.Router();
 
 adminRouter.get('/coaches', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getCoaches);
+adminRouter.post('/coaches', authenticate.verifyUser, authenticate.verifyAdmin, adminController.addCoach);
+adminRouter.put('/coaches/:coachId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.updateCoach);
+adminRouter.delete('/coaches/:coachId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.deleteCoach);
+
+adminRouter.get('/players', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getPlayers);
+// adminRouter.get('/players/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.getPlayer);
+adminRouter.put('/players/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.updatePlayer);
+adminRouter.delete('/players/:playerId', authenticate.verifyUser, authenticate.verifyAdmin, adminController.deletePlayer);
 
 module.exports = adminRouter;
