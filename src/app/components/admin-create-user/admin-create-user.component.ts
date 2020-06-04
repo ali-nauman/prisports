12c21cbf0b93fb624f4fe3d1ef3f56cea48fad9b
Hanzla Haroon <hanzla.haroon@outlook.com>
2020-06-04 19:06:57 +0500

    implemented admin-create-user

--- src/app/components/admin-create-user/admin-create-user.component.ts
+++ src/app/components/admin-create-user/admin-create-user.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-admin-create-user',
   templateUrl: './admin-create-user.component.html',
-  styleUrls: ['./admin-create-user.component.css']
+  styleUrls: ['./admin-create-user.component.css'],
 })
 export class AdminCreateUserComponent implements OnInit {
+  displaying: string;
+  registrationForm: FormGroup;
+  sport = this.formBuilder.group({
+    name: [''],
+    rank: [''],
+  });
 
-  constructor() { }
+  constructor(
+    public modal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private restService: RestService
+  ) {}
 
   ngOnInit(): void {
+    this.registrationForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      emailAddress: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      password: ['', Validators.required],
+      sports: this.formBuilder.array([
+        this.formBuilder.group({
+          name: [''],
+          rank: [''],
+        }),
+      ]),
+    });
   }
 
+  register(): void {
+      this.restService
+        .registerUser(this.registrationForm.value)
+        .subscribe(this.modal.close);
+  }
+
+  get sportForms(): FormArray {
+    return this.registrationForm.get('sports') as FormArray;
+  }
+
+  addSport(): void {
+    this.sportForms.push(this.sport);
+  }
+
+  deleteSport(i: number): void {
+    this.sportForms.removeAt(i);
+  }
 }
