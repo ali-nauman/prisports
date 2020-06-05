1f0523814ab97203f02d79c4736c61d7845df5e8
alinauman97 <alinauman97@outlook.com>
2020-06-05 22:35:28 +0500

    Updated the component to partially meet the requirements given

--- src/app/components/player-matches/player-matches.component.ts
+++ src/app/components/player-matches/player-matches.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from 'src/app/services/rest.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-player-matches',
@@ -7,14 +8,42 @@ import { RestService } from 'src/app/services/rest.service';
   styleUrls: ['./player-matches.component.css']
 })
 export class PlayerMatchesComponent implements OnInit {
-  data: any;
+  matches: any;
 
-  constructor(private restService: RestService) { }
+  constructor(
+    private authService: AuthenticationService,
+    private restService: RestService) { }
 
   ngOnInit(): void {
-    this.restService.getPlayerMatches().subscribe(
-      res => this.data = res,
-      err => console.error(err));
+    this.restService.getPlayerMatches()
+      .subscribe(res => {
+        this.matches = this.processRows(res);
+      },
+        err => console.error(err));
   }
 
+  processRows(res: any): any[] {
+    let userId = this.authService.getUserId();
+    let opponent;
+    let newRows = [];
+
+    res.forEach(row => {
+      if (row.playerAId._id == userId) {
+        opponent = row.playerBId;
+      }
+      else {
+        opponent = row.playerAId;
+      }
+
+      newRows.push({
+        opponent: opponent,
+        coach: row.coachId,
+        court: row.courtId,
+        startTime: row.startTime,
+        endTime: row.endTime
+      })
+    });
+
+    return newRows;
+  }
 }
--- src/app/components/player-practice-sessions/player-practice-sessions.component.ts
+++ src/app/components/player-practice-sessions/player-practice-sessions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from 'src/app/services/rest.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-player-practice-sessions',
@@ -7,13 +8,42 @@ import { RestService } from 'src/app/services/rest.service';
   styleUrls: ['./player-practice-sessions.component.css']
 })
 export class PlayerPracticeSessionsComponent implements OnInit {
-  data: any;
+  practiceSessions: any;
 
-  constructor(private restService: RestService) { }
+  constructor(
+    private authService: AuthenticationService,
+    private restService: RestService) { }
 
   ngOnInit(): void {
-    this.restService.getPlayerPracticeSessions().subscribe(
-      res => this.data = res,
-      err => console.error(err));
+    this.restService.getPlayerPracticeSessions()
+      .subscribe(res => {
+        this.practiceSessions = this.processRows(res);
+      },
+        err => console.error(err));
+  }
+
+  processRows(res: any): any[] {
+    let userId = this.authService.getUserId();
+    let opponent;
+    let newRows = [];
+
+    res.forEach(row => {
+      if (row.playerAId._id == userId) {
+        opponent = row.playerBId;
+      }
+      else {
+        opponent = row.playerAId;
+      }
+
+      newRows.push({
+        opponent: opponent,
+        coach: row.coachId,
+        court: row.courtId,
+        startTime: row.startTime,
+        endTime: row.endTime
+      })
+    });
+
+    return newRows;
   }
 }
