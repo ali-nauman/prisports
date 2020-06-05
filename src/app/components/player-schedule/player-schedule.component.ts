b6c5dee5b0e5e2e28855c94c1d34adc71a04510a
alinauman97 <alinauman97@outlook.com>
2020-06-05 22:36:01 +0500

    Updated the component to meet the requirements

--- src/app/components/player-schedule/player-schedule.component.ts
+++ src/app/components/player-schedule/player-schedule.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from 'src/app/services/rest.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-player-schedule',
@@ -7,18 +8,46 @@ import { RestService } from 'src/app/services/rest.service';
   styleUrls: ['./player-schedule.component.css']
 })
 export class PlayerScheduleComponent implements OnInit {
-  data: any;
-  data2: any;
-  constructor(private restService: RestService) { }
+  practiceSessions: any;
+  matches: any;
+
+  constructor(
+    private authService: AuthenticationService,
+    private restService: RestService) { }
 
   ngOnInit(): void {
-    this.restService.getPlayerPracticeSchedule().subscribe(
-      res => this.data = res,
-      err => console.error(err));
-    this.restService.getPlayerMatchSchedule().subscribe(
-        res => this.data2 = res,
-        err => console.error(err));
+    this.restService.getPlayerPracticeSchedule()
+      .subscribe(res => {
+        this.practiceSessions = this.processRows(res);
+      }, err => console.error(err));
+    this.restService.getPlayerMatchSchedule()
+      .subscribe(res => {
+        this.matches = this.processRows(res);
+      }, err => console.error(err));
   }
- 
 
+  processRows(res: any): any[] {
+    let userId = this.authService.getUserId();
+    let opponent;
+    let newRows = [];
+
+    res.forEach(row => {
+      if (row.playerAId._id == userId) {
+        opponent = row.playerBId;
+      }
+      else {
+        opponent = row.playerAId;
+      }
+
+      newRows.push({
+        opponent: opponent,
+        coach: row.coachId,
+        court: row.courtId,
+        startTime: row.startTime,
+        endTime: row.endTime
+      })
+    });
+
+    return newRows;
+  }
 }
