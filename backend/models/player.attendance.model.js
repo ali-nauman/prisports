ec58c2c65aa4253022222551a267ad522cc9f598
alinauman97 <alinauman97@outlook.com>
2020-06-05 23:18:36 +0500

    Added ref to Sport

--- backend/models/player.attendance.model.js
+++ backend/models/player.attendance.model.js
@@ -6,7 +6,7 @@ const ObjectId = mongoose.Types.ObjectId;
 const attendanceSchema = new Schema({
     playerId: { type: ObjectId, ref: 'User', required: true },
     checkinTime: { type: Date, required: true },
-    sports: [{ type: ObjectId, required: true }],
+    sports: [{ type: ObjectId, ref: 'Sport', required: true }],
 });
 
 module.exports = mongoose.model('PlayerAttendance', attendanceSchema);
